de2a2456614bacbaf2b3ca0145a1062f08af0003
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-03-21 17:47:22 +0100

    The great update

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WEEEtofono
+
 Il citofono più brutto che sia mai esistito ma quanto meno funziona asd.
 
+weeedong.wav era un suono nel pubblico dominio da freesound.org modificato con Audacity. Yeeee.
+
+## Install
+
+```
+pipenv shell
+pipenv install
+```
+
+Then run `TOKEN=123456:f00b4rf00b4rf00b4rf00b4r pipenv run weeetofono.py`.
+
+Replace the TOKEN variable with your real token. Same thing for the exaple system service.
--- weeetofono.py
+++ weeetofono.py
@@ -1,10 +1,22 @@
 import os
+# noinspection PyPackageRequirements
 from telegram.ext import Updater, CommandHandler
+import simpleaudio
+
+campanello = None
 
 
-def suona(bot, update):
-    update.message.reply_text("Hai suonato il citofono. Se sei all'ingresso 2 aspetta che qualcuno ti apra, se non lo sei vai e suona di nuovo.")
-    os.popen('./ring.sh')
+def suona(update, context):
+    global campanello
+    update.message.reply_text("Hai suonato il citofono.\n\
+    Se sei all'ingresso 2 aspetta che qualcuno ti apra, se non lo sei vai e suona di nuovo.")
+    # os.popen('./ring.sh')
+    if campanello is not None and campanello.is_playing():
+        campanello.stop()
+    campanello = wave_obj.play()
+
+
+wave_obj = simpleaudio.WaveObject.from_wave_file("weeedong.wav")
 
 updater = Updater(os.environ['TOKEN'])
 
@@ -12,4 +24,3 @@ updater.dispatcher.add_handler(CommandHandler('suona', suona))
 
 updater.start_polling()
 updater.idle()
-
